41563f66847f0f08b55ef73909ec0232d1610f0f
pavankakarla-912 <pavankakarla912@gmail.com>
2026-08-06 20:19:25 +0530

    Add IP validation in Multiples.java

---
+++ Multiples.java
@@ -0,0 +1,29 @@
+package day13;
+
+import java.util.Scanner;
+
+public class Multiples {
+    public static void main(String[] args) {
+        try (Scanner scan = new Scanner(System.in)){
+        String ip = "";
+        int part = 0,cur=0;
+        try{
+            System.out.println("Enter the IP to validate");
+            ip = scan.next();
+            String[] splited = ip.split("\\.");
+            while (part<4){
+                cur = Integer.parseInt(splited[part]);
+                if(cur<0||cur>=256)
+                {System.out.println("Invalid IP");return;}
+            part++;    
+            }
+            System.out.println("IP is valid");
+        }catch(NumberFormatException | ArrayIndexOutOfBoundsException e){
+            if(e.getClass().getName()=="java.lang.NumberFormatException")
+                System.out.println("IP in numerical");
+            else System.out.println("IP should be 4 portion of numbers");
+        }
+        scan.close();
+    }
+  }
+}
