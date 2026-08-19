71e81b379a2e05d0d8f9ccac0bfe31e57758b287
pavankakarla-912 <pavankakarla912@gmail.com>
2026-08-19 21:17:18 +0530

    Implement factorial function in Solution class

---
+++ factorial.java
@@ -0,0 +1,12 @@
+class Solution {
+    int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        int result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+}
