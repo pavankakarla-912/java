99b6f42126cd4c03357d61558f16c4ba1775095b
pavankakarla-912 <pavankakarla912@gmail.com>
2026-08-25 22:12:44 +0530

    Add Numbermirror.java to read and print an integer

---
+++ Numbermirror.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Codechef {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            System.out.println(n);
+        }
+        scanner.close();
+    }
+}
